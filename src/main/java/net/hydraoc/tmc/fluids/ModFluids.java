e64e2ae13238b297e7db78a65c9631348c8e4db7
HydraOC <bburkard834@gmail.com>
2025-07-03 12:01:50 -0500

    Getting started on implementation

--- src/main/java/net/hydraoc/tmc/fluids/ModFluids.java
+++ src/main/java/net/hydraoc/tmc/fluids/ModFluids.java
@@ -6,4 +6,6 @@ import slimeknights.mantle.registration.object.FlowingFluidObject;
 public class ModFluids {
 
     public static FlowingFluidObject<ForgeFlowingFluid> MOLTEN_FIRE_DRAGONSTEEL;
+    public static FlowingFluidObject<ForgeFlowingFluid> MOLTEN_ICE_DRAGONSTEEL;
+    public static FlowingFluidObject<ForgeFlowingFluid> MOLTEN_LIGHTNING_DRAGONSTEEL;
 }
---
+++ src/main/java/net/hydraoc/tmc/tconstruct/materials/MaterialIds.java
@@ -0,0 +1,9 @@
+package net.hydraoc.tmc.tconstruct.materials;
+
+import slimeknights.tconstruct.library.materials.definition.MaterialId;
+
+
+public class MaterialIds {
+
+    public static final MaterialId fireSteel = id("fire_dragonsteel");
+}
